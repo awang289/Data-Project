605daeeeb5d892d27227816d15b700907d20e2a6
Andreas <awang289@gmail.com>
2014-09-16 12:31:27 -0400

    work done

--- mets.py
+++ mets.py
@@ -8,8 +8,20 @@ def home():
     data = inStream.read()
     inStream.close()
     rows = data.split("\n")
+    for i in range(0,len(rows)):
+        rows[i] = rows[i].split(",")
     return render_template("data.html", info = rows)
 
+
+@app.route("/analysis")
+def analysis():
+    inStream = open("averages_hw25.csv","r")
+    data = inStream.read()
+    inStream.close()
+    rows = data.split("\n")
+    for i in range(0,len(rows)):
+        rows[i] = rows[i].split(",")
+    return render_template("analysis.html", info=rows)
 if __name__=="__main__":
     app.debug=True
     app.run() 
